9ea7f123276571519728cbe763b8ba4401922dce
PolAleks <aa.polushkin@yandex.ru>
2023-03-24 14:57:45 +0300

    Complite task with prototype pattern

---
+++ Prototype/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿namespace Prototype
+{
+    abstract class BaseClass
+    {
+        protected int id;
+        public BaseClass(int id)
+        {
+            this.id = id;
+        }
+        public void GetId()
+        {
+            Console.WriteLine($"Создан объект с Id {id}");
+        }
+        public abstract BaseClass Clone();
+    }
+}
